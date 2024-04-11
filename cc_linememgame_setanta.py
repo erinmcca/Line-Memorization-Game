85d1ad7d7dadcd2c643445cdda0a236fbeaa793d
erinmcca <erin.mccartney@stonybrook.edu>
2024-04-11 12:50:12 -0400

    option for selecting specific scene to start from, only random if no

--- cc_linememgame_setanta.py
+++ cc_linememgame_setanta.py
@@ -17,12 +17,20 @@ with open("Cu Chulainn Script (With Cuts).txt", 'r', encoding="utf8") as script:
     #print(script.readline())
 
 
-    lines = script.readlines() # TODO get rid of all empty newlines
+    all_lines = script.readlines() # TODO get rid of all empty newlines
     #print(type(lines))
     #print(lines[:20])
 
+def scene_select(lines, start=["ACT ONE\n", "Scene 1\n"]):
+    start_index = lines.index(start[0])
+    lines = lines[start_index:]
+    start_index = lines.index(start[1])
+    lines = lines[start_index:]
 
-def get_setanta_lines():
+    return lines
+
+
+def get_setanta_lines(lines):
     # there will be a more general one where the character is an argument
 
     #with open("Setanta Lines.txt", "w") as line_file:
@@ -38,6 +46,14 @@ def get_setanta_lines():
     return setanta_lines
 
 
+# maybe... in random mode, shuffle then go in order
+# for starting from a certain scene just delete everything prior
+# to differentiate acts, for act 2 first delete the first act then
+# delete prior scenes
+# that also needs to be done with char lines
+# so char lines should be made from modified lines list
+
+
 def make_line_mem_game(lines, char_lines):
     # for every line in the file get the one before it
     # then dictionary i guess
@@ -48,6 +64,7 @@ def make_line_mem_game(lines, char_lines):
     pairs = []
 
     lines.remove("\n") # how to remove all instances???
+
 
     for line in char_lines:
         i = lines.index(line)
@@ -67,12 +84,17 @@ def make_line_mem_game(lines, char_lines):
     return pairs
 
 
-def run_game(rounds, pairs):
+def run_game(rounds, pairs, randomness=True):
     count = 0
     print("Type !hint for the first two words")
     while count <= rounds:
-        r = random.randint(1, len(pairs)-1)
+
+        if(randomness):
+            r = random.randint(1, len(pairs)-1)
+        else:
+            r = count
         pair = pairs[r]
+        
         prev_line = pair[0]
         true_line = pair[1]
         #true_line = re.sub(r"[\n\t]*", "", true_line)
@@ -96,16 +118,40 @@ def run_game(rounds, pairs):
 
         count += 1
 
+     
+#start = ["ACT ONE\n", "Scene 3\n"]
 
+randomness = False
 
-setanta_lines = get_setanta_lines()
+custom = input("Do you wish to select a specific scene? Type y or n.")
+if(custom=="y"):
+    actnum = input("Which act? Type 1 or 2")
+    act = "ACT ONE\n"
+    match actnum:
+        case "1":
+            act = "ACT ONE\n"
+        case "2":
+            act = "ACT TWO \n" # whyyyyy is the formatting inconsistent
+        case _:
+            print("There's only 2 acts. I mean.... Unless you count the stuff with Fand... But I haven't written that yet.\n We're going with Act 1.")
+    scenenum = input("Which scene? Type a number from 1 to 8.")
+    scene = f'Scene {scenenum}\n'
+    start = [act, scene]
+
+else:
+    start=["ACT ONE\n", "Scene 1\n"]
+    randomness = True
+
+lines = scene_select(all_lines, start)
+
+setanta_lines = get_setanta_lines(lines)
 
 pairs = make_line_mem_game(lines, setanta_lines)
 
-print(pairs[:5])
+#print(pairs[:5])
 
 #print(list(pairs.keys())[:5])
 #print(list(pairs.values())[:5])
 
-run_game(5, pairs)
+run_game(50, pairs, randomness)
     
